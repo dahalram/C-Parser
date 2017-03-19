6c232c92bdf50229b38c03a29f82db22c75a34b9
Ram Hari Dahal <ramhari.dahal1@gmail.com>
2017-03-19 19:39:41 -0400

    Function implementation of term, rxpr and factor

--- parser.c
+++ parser.c
@@ -2,6 +2,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void factor();
+void term();
+void expr();
+
+void factor() {
+  printf("Enter <factor>\n", );
+
+  if (nextToken == IDENT || nextToken == INT_LIT) {
+    // Get the next token
+    lex();
+  } else {
+    if (nextToken == LEFT_PAREN) {
+      lex();
+      expr();
+      if (nextToken == RIGHT_PAREN) {
+        lex();
+      } else {
+        error();
+      }
+    } else {
+      error();
+    }
+  }
+  printf("Exit <factor>\n", );
+}
+
+void term() {
+  printf("Enter <term>\n");
+
+  /* Parse the first factor */
+  factor();
+
+  /* As long as the next token is * or /, get the
+   next token and parse the next factor */
+
+  while nextToken == MULTI_OP || nextToken == DIV_OP) {
+    lex();
+    factor();
+  }
+  printf("Exit <term>\n", );
+}
+
+void expr() {
+  printf("Enter <expr>\n");
+
+  /* Parse the first term */
+  term();
+
+  /* As long as the next token is + or -, get
+    the next token and parse the next term */
+  while (nextToken == ADD_OP || nextToken == SUB_OP) {
+     lex();
+     term();
+  }
+
+  printf("Exit <expr>\n");
+
+}
 
 int main() {
   FILE *f;
